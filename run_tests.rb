716039fae39828845884e95e3ee06ea2d2ec1dc0
Marijn van der Zee <marijn@serraict.com>
2020-02-18 16:04:01 +0100

    Formatting change to trigger build
    This should trigger a build that uses the gem cache. This build should be quite fast.

--- run_tests.rb
+++ run_tests.rb
@@ -1,7 +1,9 @@
 require 'html-proofer'
 
-options = { :empty_alt_ignore => true, 
+options = { 
+  :empty_alt_ignore => true, 
   :disable_external => true, 
-  :allow_hash_href => true }
+  :allow_hash_href => true 
+}
 
 HTMLProofer.check_directory("./build", options).run
